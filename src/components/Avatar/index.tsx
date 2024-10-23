5eb105bb5e7b43c8739b4ef67dac769099a553d1
Fabio <fabiofreitascmav@gmail.com>
2024-10-23 17:52:39 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/fabiofreitasd.png" alt="imagem" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/fabiofreitasd.png" alt="" />
-    <Titulo fontSize={20}>Fabio Freitas</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Fabio Freitas</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        FabioFreitasD
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
